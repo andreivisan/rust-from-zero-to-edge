5a87045370ac597b8898fa4a07970ddc5340afa2
Andrei Visan <andrei.m.visan@gmail.com>
2026-06-01 12:11:52 +0300

    Finished reversing String - optimised

--- rust-labs/rust-challenges/src/bin/reverse-string.rs
+++ rust-labs/rust-challenges/src/bin/reverse-string.rs
@@ -1,36 +1,16 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let word = input.to_string();
-    if word.len() < 2 {
-        return word;
+    if input.len() < 2 {
+        return input.to_string();
     }
-    let mut letters = input.graphemes(true).collect::<Vec<&str>>();
-    if is_palindrome(&letters) {
-        return word;
-    }
-    let mut start = 0;
-    let mut end = letters.len() - 1;
-    while start < end {
-        letters.swap(start, end);
-        start += 1;
-        end -= 1;
-    }
-    println!("{:?}", letters);
-    letters.concat()
-}
-
-pub fn is_palindrome(letters: &[&str]) -> bool {
-    let mut start = 0;
-    let mut end = letters.len() - 1;
-    while start < end {
-        if letters[start] != letters[end] {
-            return false;
+    input.graphemes(true).rev().fold(
+        String::with_capacity(input.len()), 
+        |mut reversed, grapheme| {
+            reversed.push_str(grapheme);
+            reversed
         }
-        start += 1;
-        end -= 1;
-    }
-    true
+    )
 }
 
 fn main() {
