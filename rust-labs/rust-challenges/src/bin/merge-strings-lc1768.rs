92c30fd414086f6e3e40dfe06ade6722ca73cb7f
Andrei Visan <andrei.m.visan@gmail.com>
2026-06-09 10:37:47 +0300

    Finished merge strings

--- rust-labs/rust-challenges/src/bin/merge-strings-lc1768.rs
+++ rust-labs/rust-challenges/src/bin/merge-strings-lc1768.rs
@@ -1,14 +1,20 @@
-use std::cmp;
-
 pub fn merge_alternately(word1: String, word2: String) -> String {
-    if word1.is_empty() { return word2; }
-    if word2.is_empty() { return word1; }
-    let min_len = cmp::min(word1.len(), word2.len());
-    let word1_tail = &word1[min_len..];
-    let word2_tail = &word2[min_len..];
-    word1.chars().zip(word2.chars())
-        .flat_map(|(a, b)| [a, b])
-        .chain(word1_tail)
-        .chain(word2_tail)
+    let mut a = word1.chars();
+    let mut b = word2.chars();
+    let mut result = String::with_capacity(word1.len() + word2.len());
+    loop {
+        match (a.next(), b.next()) {
+            (Some(x), Some(y)) => { result.push(x); result.push(y); }
+            (Some(x), None) => { result.push(x); result.extend(a); break; }
+            (None, Some(y)) => { result.push(y); result.extend(b); break; }
+            (None, None) => break,
+        }
+    }
+    result
 }
-fn main() {}
+
+fn main() {
+    let result = merge_alternately("Hello".to_string(), ", Andrei aka Cybermaster!".to_string());
+    println!("{result}");
+}
+
